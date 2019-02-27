5f84ef0d82966f2beceddf361ebf2dfd8fdc92ef
zhuaf <2353536563@qq.com>
2019-02-27 23:13:15 +0800

    update

--- vueShop/settings.py
+++ vueShop/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+sys.path.insert(0, BASE_DIR)
+sys.path.insert(0, os.path.join(BASE_DIR, "apps"))
+sys.path.insert(0, os.path.join(BASE_DIR, "extra_apps"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -27,6 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# AUTH_USER_MODEL = 'users.UserProFile'  # 替换系统的models——user
 
 # Application definition
 
@@ -87,6 +90,7 @@ DATABASES = {
         'PASSWORD': 'zaf123',
         'HOST': '127.0.0.1',
         'PORT': '3306',
+        'OPTIONS': {'init_command': 'SET default_storage_engine=INNODB;'}
     }
 }
 
